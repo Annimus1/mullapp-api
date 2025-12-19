364ee13ce85833a21f3636e2858aff471b69ebe6
Pablo Vergara <pablo.j98@gmail.com>
2025-12-19 13:15:49 -0400

    Initial Commit

---
+++ src/main/java/dev/pablo/mullapp/MullappApplication.java
@@ -0,0 +1,13 @@
+package dev.pablo.mullapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MullappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MullappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/pablo/mullapp/MullappApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pablo.mullapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MullappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
